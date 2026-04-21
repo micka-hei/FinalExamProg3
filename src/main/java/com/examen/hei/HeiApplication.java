f3ba5ec9804dabab35ab37e0e0570eef74bb8938
micka-hei <hei.safidy@gmail.com>
2026-04-21 13:41:30 +0300

    specification OAS

---
+++ src/main/java/com/examen/hei/HeiApplication.java
@@ -0,0 +1,13 @@
+package com.examen.hei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examen/hei/HeiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.hei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
